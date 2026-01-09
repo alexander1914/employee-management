968bcfd4b39cd19103e4d620fc4836257944a1d6
alexander1914 <alexander.oliveira99@gmail.com>
2026-01-09 18:44:50 +0000

    [Set up project] I had been creating project today

---
+++ employee-management/src/test/java/com/github/alexander/employee/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexander.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
