f5acd773e432d8b245df2e145258cd853227ebc9
alexander1914 <alexander.oliveira99@gmail.com>
2026-01-27 23:33:33 +0000

    [Entity Employee] I had been creating new employee entity and DTO today

---
+++ employee-management/src/main/java/com/github/alexander/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.github.alexander.employee.repository;
+
+import com.github.alexander.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
